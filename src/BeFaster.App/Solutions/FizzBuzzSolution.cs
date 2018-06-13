af0291dd2a9f58b60fd1e364cf100d6981734d68
sbx_user1059 <sbx_user1059@ip-10-11-241-63.eu-west-2.compute.internal>
2018-06-13 19:15:28 +0000

    Wed Jun 13 19:15:28 UTC 2018

--- src/BeFaster.App/Solutions/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FizzBuzzSolution.cs
@@ -1,4 +1,6 @@
-﻿using BeFaster.Runner.Exceptions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using BeFaster.Runner.Exceptions;
 
 namespace BeFaster.App.Solutions
 {
@@ -6,7 +8,13 @@ namespace BeFaster.App.Solutions
     {
         public static string FizzBuzz(int number)
         {
-            throw new SolutionNotImplementedException();
+            var results = new List<string>();
+            if (number % 3 == 0) results.Add("fizz");
+            if (number % 5 == 0) results.Add("buzz");
+
+            return results.Any()
+                ? string.Join(" ", results)
+                : number.ToString();
         }
     }
 }

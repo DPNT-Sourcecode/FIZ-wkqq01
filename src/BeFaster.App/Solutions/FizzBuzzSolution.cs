08e161ccd8e1a4510b65941b74a309301514089e
sbx_user1059 <sbx_user1059@ip-10-11-241-63.eu-west-2.compute.internal>
2018-06-13 19:54:13 +0000

    Wed Jun 13 19:54:13 UTC 2018

--- src/BeFaster.App/Solutions/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FizzBuzzSolution.cs
@@ -11,32 +11,23 @@ namespace BeFaster.App.Solutions
 
             var results = new List<string>();
 
-            bool fizz = false, buzz = false, deluxe = false;
+            bool fizz = false, buzz = false, deluxe = false, fake = false;
 
             if (number % 3 == 0 && numberString.Contains("3"))
-            {
                 deluxe = fizz = true;
-            }
             if (number % 5 == 0 && numberString.Contains("5"))
-            {
                 deluxe = buzz = true;
-            }
 
-            if (deluxe.Any())
-            {
-                results.Add(string.Join(" ", deluxe));
-                results.Add(number % 2 == 1 ? "fake deluxe" : "deluxe");
-            }
+            if (deluxe) fake = number % 2 == 1;
 
-            else
-            {
-                // FIZ_R5 - All the previous rules for "fizz" and "buzz" are still valid
-                // ↑ bullshit!
-                if (number % 3 == 0 || numberString.Contains("3"))
-                    results.Add("fizz");
-                if (number % 5 == 0 || numberString.Contains("5"))
-                    results.Add("buzz");
-            }
+            if (number % 3 == 0 || numberString.Contains("3"))
+                fizz = true;
+            if (number % 5 == 0 || numberString.Contains("5"))
+                buzz = true;
+
+            if (fizz) results.Add("fizz");
+            if (buzz) results.Add("buzz");
+            if (deluxe) results.Add(fake ? "fake deluxe" : "deluxe");
 
             return results.Any()
                 ? string.Join(" ", results)

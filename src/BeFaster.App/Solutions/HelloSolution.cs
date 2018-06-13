fe8012c498a1c7038d658703413197064e1f2d1a
sbx_user1059 <sbx_user1059@ip-10-11-241-63.eu-west-2.compute.internal>
2018-06-13 19:11:29 +0000

    Wed Jun 13 19:11:29 UTC 2018

--- src/BeFaster.App/Solutions/HelloSolution.cs
+++ src/BeFaster.App/Solutions/HelloSolution.cs
@@ -2,6 +2,6 @@
 {
     public static class HelloSolution
     {
-        public static string Hello(string friendName) => "Hello, World!";
+        public static string Hello(string friendName) => $"Hello, {friendName}!";
     }
 }
